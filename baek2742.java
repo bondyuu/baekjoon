05db6ed099c90f12736255d2b0f47c99b9ee102a
bondyuu <leo4902@gmail.com>
2022-07-21 18:25:01 +0900

    baek(java)

---
+++ baek2742.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class baek2742 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        sc.close();
+
+        for (int i = num; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        
+    }
+    
+}
