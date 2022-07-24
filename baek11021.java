54ead518fc30306323e438c9fa1cd0f3b2f26e09
bondyuu <leo4902@gmail.com>
2022-07-24 19:59:17 +0900

    baek(java)

---
+++ baek11021.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class baek11021 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        
+        for(int i=0;i<A;i++) {
+            int B = sc.nextInt();
+            int C = sc.nextInt();   
+
+            System.out.println("Case #"+(i+1)+": "+(B+C));
+        }
+        sc.close();         
+    }
+}
---
+++ baek2438.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class baek2438 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
